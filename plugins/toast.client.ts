da99b8eb0fc1f62a21ae9f01513807e0481037a7
AndargWorku <andargworku8240@gmail.com>
2025-10-25 16:00:03 +0300

    updat something

--- plugins/toast.client.ts
+++ plugins/toast.client.ts
@@ -1,13 +1,12 @@
 
+// plugins/toast.ts
 import { defineNuxtPlugin } from '#app'
-import ToastPkg from 'vue-toastification'
+import Toast, { type PluginOptions, POSITION } from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const Toast = ToastPkg.default || ToastPkg
-  const { POSITION, useToast } = Toast
-
-  const options = {
+  // Define custom options for the toast notifications
+  const options: PluginOptions = {
     position: POSITION.TOP_RIGHT,
     timeout: 4000,
     closeOnClick: true,
@@ -25,40 +24,6 @@ export default defineNuxtPlugin((nuxtApp) => {
     newestOnTop: true,
   }
 
+  // Register the plugin with the Vue app
   nuxtApp.vueApp.use(Toast, options)
-
-  // Optional: register `useToast` globally
-  nuxtApp.provide('toast', useToast)
 })
-
-
-
-
-// // plugins/toast.ts
-// import { defineNuxtPlugin } from '#app'
-// import Toast, { type PluginOptions, POSITION } from 'vue-toastification'
-// import 'vue-toastification/dist/index.css'
-
-// export default defineNuxtPlugin((nuxtApp) => {
-//   // Define custom options for the toast notifications
-//   const options: PluginOptions = {
-//     position: POSITION.TOP_RIGHT,
-//     timeout: 4000,
-//     closeOnClick: true,
-//     pauseOnFocusLoss: true,
-//     pauseOnHover: true,
-//     draggable: true,
-//     draggablePercent: 0.6,
-//     showCloseButtonOnHover: false,
-//     hideProgressBar: false,
-//     closeButton: 'button',
-//     icon: true,
-//     rtl: false,
-//     transition: 'Vue-Toastification__bounce',
-//     maxToasts: 5,
-//     newestOnTop: true,
-//   }
-
-//   // Register the plugin with the Vue app
-//   nuxtApp.vueApp.use(Toast, options)
-// })
